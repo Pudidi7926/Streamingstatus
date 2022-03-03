8ef4c31dbe125c1aa031b76760f6b0e6db9a1697
Pudidi7926 <100346540+Pudidi7926@users.noreply.github.com>
2022-03-03 15:11:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = commands.Bot(
 
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ 6AM - 10PM Online ┊ Late Respond ┊ Puasa bor. ┊ 7/30 Hari lagi.", url = "https://www.twitch.tv/Danish8409"))
+  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ 7:00 - 23:00 Online ┊ Fast Respond ┊ Mager Bjir. ┊ 3 Minggu Puasa Ges.", url = "https://www.twitch.tv/tukang_turu"))
 
 
 keep_alive.keep_alive()
